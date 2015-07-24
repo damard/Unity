d1cc55adde3d8496ce418dd8f1a031cb03c3f0c0
damard <dave.marchessault@gmail.com>
2015-07-24 19:18:41 -0400

    number wizard ui
    progresssss

---
+++ Number Wizard/Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public void LoadLevel(string levelName) {
+		Debug.Log("Level load requested for: " + levelName);
+		Application.LoadLevel(levelName);
+	}
+	
+	public void QuitGame() {
+		Debug.Log("Requested level quit");
+	}
+}
