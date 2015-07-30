2bc6a73c2e47f50a37580dec21840c4462426d05
damard <dave.marchessault@gmail.com>
2015-07-30 15:37:14 -0400

    commiting blockbreaker test

---
+++ Block Breaker/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	static MusicPlayer instance = null;
+
+	// Use this for initialization
+	void A () {
+
+		if (instance != null) {
+			Destroy(gameObject);
+			Debug.Log("Duplicate music player; Self-Destructing");
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+
+			AudioSource audioSource = GetComponent<AudioSource>();
+			audioSource.Play();
+		}
+	}
+}
