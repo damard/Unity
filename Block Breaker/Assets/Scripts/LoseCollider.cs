ad28941edbf408b6378a53657d24c7f0d6180602
damard <dave.marchessault@gmail.com>
2015-08-01 10:34:37 -0400

    block breaker progress

---
+++ Block Breaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+	
+	public LevelManager levelManager;
+
+	void OnTriggerEnter2D (Collider2D collider) {
+		levelManager.LoadLevel ("Lose Screen");
+	}
+}
---
+++ Block Breaker/Assets/Scripts/PaddleController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleController : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	
+	void Awake () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16f;
+		print (mousePosInBlocks);
+	}
+}
